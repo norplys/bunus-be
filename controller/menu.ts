d29b4c6c161e6c8293cc53bc316d56e3978709a8
norplys <joywinata88@gmail.com>
2024-01-24 08:46:07 +0700

    add prettier

--- controller/menu.ts
+++ controller/menu.ts
@@ -1,18 +1,14 @@
 import { getAllMenu } from "../repositories/menu";
 import { Request, Response } from "express";
 
+const getMenus = async (req: Request, res: Response) => {
+  const { id } = <{ id: string | undefined }>req.query;
+  const menus = await getAllMenu(id);
+  res.status(200).json({
+    status: "Success",
+    message: "Successfully Retrieve Menus",
+    data: menus,
+  });
+};
 
-const getMenus = async (req : Request, res : Response) => {
-    const { id }  = <{id:string | undefined}> req.query
-    const menus = await getAllMenu(id)
-    res.status(200).json({
-        status : 'Success',
-        message : 'Successfully Retrieve Menus',
-        data : menus
-    })
-
-}
-
-export {
-    getMenus
-}
+export { getMenus };
